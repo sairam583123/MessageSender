cf58a3393c89054a62a883e46a9ef97386d70e33
sairam <sairamsai583@gmail.com>
2021-11-14 12:56:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from tkinter import *
+from tkinter.messagebox import showinfo, showerror
+
+def send_sms(number, message):
+    url = 'https://www.fast2sms.com/dev/bulk'
+    params = {
+        'authorization': '0N9FULbympMEDPAzqYj7ZX2TloGWxkgcJr4e8Bi5nVhvIafws6ucvbT8hUqlIZ9L7n46DWGfgyBixV0t',
+        'sender_id': 'FSTSMS',
+        'message': message,
+        'language': 'english',
+        'route': 'p',
+        'numbers': number
+    }
+    response = requests.get(url, params=params)
+    dic = response.json()
+    print(dic)
+    return dic.get('return')
+
+
+def btn_click():
+    num = textNumber.get()
+    msg = textMsg.get("1.0", END)
+    r = send_sms(num, msg)
+    if r:
+        showinfo("Send Success", "Successfully sent")
+    else:
+        showerror("Error", "provide 10 digit number")
+
+
+# Creating GUI
+root = Tk()
+root.title("Message Sender ")
+root.geometry("400x550")
+font = ("Helvetica", 22, "bold")
+textNumber = Entry(root, font=font)
+textNumber.pack(fill=X, pady=20)
+textMsg = Text(root)
+textMsg.pack(fill=X)
+sendBtn = Button(root, text="SEND SMS", command=btn_click)
+sendBtn.pack()
+root.mainloop()
